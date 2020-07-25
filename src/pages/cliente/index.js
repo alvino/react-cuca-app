531703791a943bb48299710a52b2d9fc2071cfcf
Alvino Figueiredo <alvino@pop-os.localdomain>
2020-07-24 22:24:18 -0300

    criando o index para acessa o route de cada modulo

--- src/pages/cliente/index.js
+++ src/pages/cliente/index.js
@@ -1,102 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table'
-import { Link } from 'react-router-dom'
-import { toast } from 'react-toastify';
-import api from '../../server/api'
+import React from "react";
+import {
+  Switch,
+  Route,
+  useRouteMatch,
+} from "react-router-dom";
 
+
+import NavBar from '../../components/NavBar'
+import Home from './home'
+import ClientRegister from './register'
+import ClientDelete from './delete'
 
 export default () => {
 
-  const [clients, setClients] = useState([]);
-  const [rowSelected, setRowSelected] = useState({ row: {}, isSelected: false })
-
-  useEffect(() => {
-    async function fetchData() {
-      const response = await api.get("/client")
-      setClients(response.data.clients, { autoClose: 2000 })
-    }
-    fetchData()
-  }, []);
-
-  function onRowSelect(row, isSelected) {
-    setRowSelected({ row, isSelected })
-  }
-
-  // function onSelectAll(isSelected) {
-  //   console.log(`is select all: ${isSelected}`);
-  // }
-
-  const selectRow = {
-    mode: "radio",//"checkbox",
-    // clickToSelect: true,
-    // selected: [], // default select on table
-    bgColor: "rgba(0,123,255,.4)",
-    onSelect: onRowSelect,
-    // onSelectAll: onSelectAll,
-  }
-
+  let { path } = useRouteMatch();
 
   return (
-    <div >
 
-      <ul className="nav justify-content-center p-2">
-        <li className="nav-item m-2">
-          <Link to="/cliente/register" className="nav-link btn-primary">
-            Cadastra Cliente
-        </Link>
-        </li>
-        {
-          rowSelected.isSelected ?
-            <li className="nav-item m-2">
-              <Link to={`/cliente/register/${rowSelected.row.id}`} className="nav-link btn-info">
-                Alterar Cliente
-              </Link>
-            </li>
-            : ""
-        }
-        {
-          rowSelected.isSelected ?
-            <li className="nav-item m-2">
-              <Link to={`/cliente/delete/${rowSelected.row.id}`} className="nav-link btn-danger" >
-                Deletar Cliente
-              </Link>
-            </li>
-            : ""
-        }
+    <div>
+      <NavBar />
 
-      </ul>
-
-      <BootstrapTable
-        version="4"
-        data={clients}
-        selectRow={selectRow}
-        pagination
-        search
-        hover
-        exportCSV
-        csvFileName="table-clientes.csv"
-        ignoreSinglePage
-      >
-
-        <TableHeaderColumn dataField="name" dataSort>
-          Name
-        </TableHeaderColumn>
-        <TableHeaderColumn dataField="email" dataSort>
-          Email
-        </TableHeaderColumn>
-        <TableHeaderColumn dataField="telephone" dataSort>
-          Telefone
-        </TableHeaderColumn>
-        <TableHeaderColumn dataField="cpf" isKey={true} dataSort>
-          CPF
-        </TableHeaderColumn>
-        <TableHeaderColumn dataField="city" dataSort>
-          Cidade
-        </TableHeaderColumn>
-        <TableHeaderColumn dataField="uf" dataSort>
-          Uf
-        </TableHeaderColumn>
-      </BootstrapTable>
-    </div >
-  );
+      <div className='container-fluid'>
+        <main>
+          <Switch>
+            <Route exact path={path}>
+              <Home />
+            </Route>
+            <Route exact path={`${path}/register`}>
+              <ClientRegister />
+            </Route>
+            <Route exact path={`${path}/register/:id`}>
+              <ClientRegister />
+            </Route>
+            <Route exact path={`${path}/delete/:id`}>
+              <ClientDelete />
+            </Route>
+          </Switch>
+        </main>
+      </div>
+    </div>
+  )
 }
