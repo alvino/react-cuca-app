1481673b7fbbac53b3039611e33ae59352425ab4
Alvino Figueiredo <alvino@pop-os.localdomain>
2020-07-24 22:23:06 -0300

    criando navbar para acesso no nav

---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {
+    NavLink,
+} from "react-router-dom";
+
+import Header from './Header'
+
+export default function NavBar() {
+    return (
+        <div className="sticky-top">
+            <Header>
+                <li className="nav-item">
+                    <NavLink activeClassName='active' className="nav-link" to="/cliente">Cliente</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink activeClassName='active' className="nav-link" to="/fornecedor">Fornecedor</NavLink>
+                </li>
+            </Header>
+        </div>
+    )
+}
