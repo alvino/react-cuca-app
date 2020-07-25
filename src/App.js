e05ea30cc3907bcacf22edd9ce8a40ffa5999468
Alvino Figueiredo <alvino@pop-os.localdomain>
2020-07-24 22:22:03 -0300

    enxugando app para route dos modulos

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import {
 	Switch,
 	Route,
 	Redirect,
-	Link,
 } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
@@ -13,16 +12,10 @@ import "react-toastify/dist/ReactToastify.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "react-bootstrap-table/dist/react-bootstrap-table-all.min.css";
 
-
-import Header from "./components/Header";
-// import Footer from "./components/Footer";
-
+import Home from './pages/home'
 import Client from './pages/cliente'
-import ClientRegister from './pages/cliente/register'
-import ClientDelete from './pages/cliente/delete'
-
 import Provider from './pages/fornecedor'
-import ProviderRegister from './pages/fornecedor/register'
+import PDV from './pages/venda'
 
 
 function App() {
@@ -30,58 +23,22 @@ function App() {
 		<>
 			<ToastContainer ></ToastContainer>
 			<Router>
-				<div>
-					<Header>
-						<li className="nav-item">
-							<Link className="nav-link" to="/cliente">Cliente</Link>
-						</li>
-						<li className="nav-item">
-							<Link className="nav-link" to="/fornecedor">Fornecedor</Link>
-						</li>
-					</Header>
+				<Switch>
+					<Route exact path="/">
+						<Home />
+					</Route>
+					<Route path="/cliente">
+						<Client />
+					</Route>
+					<Route path="/fornecedor">
+						<Provider />
+					</Route>
+					<Route path='/pdv'>
+						<PDV />
+					</Route>
+					<Redirect to='/' />
+				</Switch>
 
-					<div className='container-fluid'>
-						<main>
-							<Switch>
-								<Route exact path="/">
-									<></>
-								</Route>
-
-								<Route exact path="/cliente">
-									<Client />
-								</Route>
-								<Route exact path="/cliente/register">
-									<ClientRegister />
-								</Route>
-								<Route exact path="/cliente/register/:id">
-									<ClientRegister />
-								</Route>
-								<Route exact path="/cliente/delete/:id">
-									<ClientDelete />
-								</Route>
-
-								<Route exact path="/fornecedor">
-									<Provider />
-								</Route>
-								<Route path="/fornecedor/register">
-									<ProviderRegister />
-								</Route>
-
-								<Route exact path="/produto">
-									<Client />
-								</Route>
-								<Route path="/produto/register">
-									<ClientRegister />
-								</Route>
-
-
-								<Redirect to='/' />
-							</Switch>
-						</main>
-					</div>
-
-
-				</div>
 			</Router>
 		</>
 	)
