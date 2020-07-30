fa1a951a1de5de08ace41fea0bb81336cc1075e3
Alvino Figueiredo <alvino@pop-os.localdomain>
2020-07-30 16:37:13 -0300

    remodelado para adicionar o style.css dando um efeito de zoom.

--- src/components/Box/index.js
+++ src/components/Box/index.js
@@ -1,22 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default function Box(props) {
+import './style.css'
+
+export default function (props) {
     return (
         <Link
             to={props.to}
-            className="badge badge-primary m-3 shadow text-white"
-            style={{
-                backgroundColor: 'rgba(0, 123, 255,0.7)',
-            }}
+            className="box d-flex m-3 rounded-lg shadow-lg text-white"
         >
 
             <div
                 className='d-flex justify-content-center align-items-center'
-                style={{
-                    width: '13rem',
-                    height: '13rem',
-                }}>
+            >
                 <h3>
                     {props.title}
                 </h3>
