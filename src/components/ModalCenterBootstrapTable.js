9b94a7765cfd0ceb73256c6185c18cae0c70a6ef
Alvino Figueiredo <alvino@pop-os.localdomain>
2020-07-30 16:39:25 -0300

    criando um componente modal

---
+++ src/components/ModalCenterBootstrapTable.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { BootstrapTable } from 'react-bootstrap-table'
+import {
+    Modal
+} from 'react-bootstrap'
+
+
+export default function ModalCenterBootstrapTable(props) {
+    function onRowSelect(row, isSelected) {
+        props.onSelected({ row, isSelected })
+        props.onHide()
+    }
+
+    const selectRow = {
+        mode: "radio",//"checkbox",
+        bgColor: "rgba(0,123,255,.4)",
+        onSelect: onRowSelect,
+    }
+
+    return (
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter"> Pesquisa de Produto </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <BootstrapTable
+                    version="4"
+                    data={props.dataList}
+                    selectRow={selectRow}
+                    pagination
+                    search
+                    hover
+                    ignoreSinglePage
+                >
+
+                    {props.children}
+
+                </BootstrapTable>
+            </Modal.Body>
+        </Modal>
+    );
+}
