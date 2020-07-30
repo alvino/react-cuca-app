f0f437317acb635dbd6273c8ef4c7e8b2c7a0e43
Alvino Figueiredo <alvino@pop-os.localdomain>
2020-07-30 16:39:03 -0300

    adicionando page estoque e configurando o toastcontainer

--- src/App.js
+++ src/App.js
@@ -9,19 +9,25 @@ import { ToastContainer } from "react-toastify";
 
 
 import "react-toastify/dist/ReactToastify.css";
-import "bootstrap/dist/css/bootstrap.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "react-bootstrap-table/dist/react-bootstrap-table-all.min.css";
+
 
 import Home from './pages/home'
 import Client from './pages/cliente'
 import Provider from './pages/fornecedor'
+import Estoque from './pages/estoque'
 import PDV from './pages/venda'
 
 
 function App() {
 	return (
 		<>
-			<ToastContainer ></ToastContainer>
+			<ToastContainer
+				position="bottom-right"
+				hideProgressBar
+				closeOnClick
+			/>
 			<Router>
 				<Switch>
 					<Route exact path="/">
@@ -32,6 +38,9 @@ function App() {
 					</Route>
 					<Route path="/fornecedor">
 						<Provider />
+					</Route>
+					<Route path="/estoque">
+						<Estoque />
 					</Route>
 					<Route path='/pdv'>
 						<PDV />
