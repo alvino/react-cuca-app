d781da703bcc1259c5c0e2eb379e4dfa5af2ed2c
alvino figueiredo <crazyduckalvino@gmail.com>
2020-08-20 14:24:55 -0300

    home do projeto no estilo box com image

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,14 @@
 import React from "react";
+import {
+  FaUser,
+  FaTruck,
+  FaWarehouse,
+  FaShoppingCart,
+  FaClipboardList,
+  FaDollarSign,
+  FaPlus,
+  FaMinus,
+} from 'react-icons/fa'
 
 // import NavBar from '../components/NavBar'
 import Header from '../components/Header'
@@ -6,17 +16,50 @@ import Box from '../components/Box'
 
 export default () => {
 
-
   return (
-    <div>
+    <div className='mb-5'>
       <Header />
 
       <div className='container-fluid'>
-        <main className='mt-5 d-flex justify-content-center align-items-center'>
-          <Box title='Cliente' to='/cliente' />
-          <Box title='Fornecedor' to='/fornecedor' />
-          <Box title='Estoque' to='/estoque' />
-          <Box title='Ponto de Venda' to='/pdv' />
+        <main className='d-flex sm:flex-column  align-items-center'>
+
+          <div className='row w-75 mx-auto'>
+
+            <Box title='Ponto de Venda' to='/venda' >
+              <FaShoppingCart size='110px' />
+            </Box>
+
+            <Box title='Orçamentos' to='/orcamento' >
+              <FaClipboardList size='110px' />
+            </Box>
+
+            <Box title='Estoque' to='/estoque' >
+              <FaWarehouse size='110px' />
+            </Box>
+
+            <Box title='Clientes' to='/cliente' >
+              <FaUser size='110px' />
+            </Box>
+
+            <Box title='Fornecedores' to='/fornecedor' >
+              <FaTruck size='110px' />
+            </Box>
+
+            <Box title='Entrada' to='/entrada' >
+              <FaDollarSign size='110px' />
+              <FaPlus size='40px' />
+            </Box>
+
+            <Box title='Saida' to='/entrada' >
+              <FaDollarSign size='110px' />
+              <FaMinus size='40px' />
+            </Box>
+
+
+
+
+          </div>
+
         </main>
       </div>
     </div>
