43069b60b81d83aea3e670b88a713e7f582e2d56
alvino figueiredo <crazyduckalvino@gmail.com>
2020-08-20 14:21:57 -0300

    criando compomentes para formata os valores em float para espressao de valores

---
+++ src/components/NumberFormat.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import CurrencyFormat from 'react-currency-format'
+
+export default function NumberFormat(props) {
+    return (
+        <CurrencyFormat  {...props} />
+    )
+}
+
+
+NumberFormat.defaultProps = {
+    displayType: 'text',
+    prefix: 'R$ ',
+    decimalSeparator: ',',
+    thousandSeparator: '.',
+    decimalScale: 2,
+    fixedDecimalScale: true,
+}
+
+
+NumberFormat.propTypes = {
+    value: PropTypes.object.isRequired
+}
---
+++ src/components/bootstrap/InputNumberFormat.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Form, InputGroup } from 'react-bootstrap'
+import CurrencyFormat from 'react-currency-format'
+
+export default function InputNumberFormat(props) {
+  const { id, label, prefix, ...restProps } = props
+  return (
+    <Form.Group controlId={id}>
+      <Form.Label>{label}</Form.Label>
+      {
+        prefix === '' ?
+          <CurrencyFormat  {...restProps} />
+          :
+          <InputGroup className="mb-3">
+            <InputGroup.Prepend>
+              <InputGroup.Text >{prefix}</InputGroup.Text>
+            </InputGroup.Prepend>
+            <CurrencyFormat  {...restProps} />
+          </InputGroup>
+      }
+
+
+
+    </Form.Group>
+  )
+}
+
+
+InputNumberFormat.defaultProps = {
+  className: 'form-control form-control-lg',
+  type: 'text',
+  prefix: 'R$ ',
+  decimalSeparator: ',',
+  thousandSeparator: '.',
+}
+
+
+InputNumberFormat.propTypes = {
+  name: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired
+}
