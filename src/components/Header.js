b4cdaa2dfb4c571e59c56847abafa809ee7b8877
alvino figueiredo <crazyduckalvino@gmail.com>
2020-08-24 16:04:38 -0300

    fazendo correção de <a>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,23 @@
 import React from "react";
-import { Link } from 'react-router-dom'
-import {
-  Navbar,
-  Nav,
-} from 'react-bootstrap'
+import { Link } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
 
-
-import logoCuca from '../assert/logo_cuca.svg'
-
+import logoCuca from "../assert/logo_cuca.svg";
 
 export default function Header(props) {
-
   return (
-    <div className="sticky-top mb-5">
-
-      <Navbar bg="primary" variant="dark" className='shadow'>
-        <Navbar.Brand href="#home">
-          <Link to='/'>
-            <img src={logoCuca} width={props.logoWidth} alt="Logo" />
-          </Link>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            {props.children}
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-
-    </div>
-  )
+    <Navbar bg="primary" variant="dark" className="shadow sticky-top mb-5">
+      <Link to="/" className='navbar-brand'>
+        <img src={logoCuca} width={props.logoWidth} alt="Logo" />
+      </Link>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">{props.children}</Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
 
 Header.defaultProps = {
-  logoWidth: '180px'
-}
+  logoWidth: "180px",
+};
