6dc199d77c94759bf7ec526146c9efde9bd36e10
alvino figueiredo <crazyduckalvino@gmail.com>
2020-08-27 09:05:50 -0300

    removendo o import do css do react-bootstrap-table

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ import { ToastContainer } from "react-toastify";
 
 import "react-toastify/dist/ReactToastify.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "react-bootstrap-table/dist/react-bootstrap-table-all.min.css";
 
 
 import Home from './pages/home'
