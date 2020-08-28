e6b2799334f04b91efc3bb0f8ad201aa780a4467
alvino figueiredo <crazyduckalvino@gmail.com>
2020-08-28 07:57:48 -0300

    alterando defaultProps e propTypes

--- src/components/bootstrap/InputFormControl.jsx
+++ src/components/bootstrap/InputFormControl.jsx
@@ -1,41 +1,29 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-
-
-export default function InputFormControl(props) {
-    const { children, ...restProps } = props
-    return (
-        <div className='form-group'>
-            <label htmlFor={restProps.id}>{restProps.label}</label>
-            {
-                children ?
-                    (<div className="input-group mb-3">
-                        <input
-                            {...restProps}
-                        />
-                        <div className="input-group-append">
-                            {children}
-                        </div>
-                    </div>)
-                    :
-                    (<input
-                        {...restProps}
-                    />)
-            }
-        </div>
-
-    )
-}
-
+import React from "react";
+import PropTypes from "prop-types";
 
 InputFormControl.defaultProps = {
-    className: 'form-control form-control-lg ',
-    type: 'text',
-}
-
+  className: "form-control form-control-lg ",
+  type: "text",
+};
 
 InputFormControl.propTypes = {
-    name: PropTypes.string.isRequired,
-    id: PropTypes.string.isRequired,
-    label: PropTypes.string.isRequired
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+};
+
+export default function InputFormControl(props) {
+  const { children, label, ...restProps } = props;
+  return (
+    <div className="form-group">
+      <label htmlFor={restProps.id}>{label}</label>
+      {children ? (
+        <div className="input-group mb-3">
+          <input {...restProps} />
+          <div className="input-group-append">{children}</div>
+        </div>
+      ) : (
+        <input {...restProps} />
+      )}
+    </div>
+  );
 }
