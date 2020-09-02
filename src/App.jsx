62c2ccaa97b8358b02a455ed09dcc8b7dd04ebed
alvino figueiredo <crazyduckalvino@gmail.com>
2020-09-02 08:08:32 -0300

    alterando para utilizar o sass

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer } from "react-toastify";
 
 
 import "react-toastify/dist/ReactToastify.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./styles/App.scss";
 
 
 import Home from './pages/home'
