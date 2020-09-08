32361cbaacfbb3494ef0905755df6138e0d7271c
alvino figueiredo <crazyduckalvino@gmail.com>
2020-09-08 20:34:00 -0300

    alterando para jsx nativos

--- src/components/bootstrap/Buttons.jsx
+++ src/components/bootstrap/Buttons.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { Button } from "react-bootstrap";
 
 export const ButtonHandlePrint = () => (
-  <Button onClick={() => window.print()}>Imprimir</Button>
+  <button className="btn btn-primary" onClick={() => window.print()}>Imprimir</button>
 );
