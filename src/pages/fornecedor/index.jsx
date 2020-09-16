d864ef1dacd0563eb16b7026ca9c46168ef8d8e4
alvino figueiredo <crazyduckalvino@gmail.com>
2020-09-16 08:57:02 -0300

    alterando

--- src/pages/fornecedor/index.jsx
+++ src/pages/fornecedor/index.jsx
@@ -1,25 +1,19 @@
 import React from "react";
-import {
-  Switch,
-  Route,
-  useRouteMatch,
-} from "react-router-dom";
+import { Switch, Route, useRouteMatch } from "react-router-dom";
 
-import NavBar from '../../components/NavBar'
-import Home from './home'
-import Register from './register'
-import Delete from './delete'
+import NavBar from "../../components/NavBar";
+import Home from "./home";
+import Register from "./register";
+import Delete from "./delete";
 
 export default () => {
-
   let { path } = useRouteMatch();
 
   return (
-
     <div>
       <NavBar />
 
-      <div className='container-fluid'>
+      <div className="container-fluid">
         <main>
           <Switch>
             <Route exact path={path}>
@@ -37,7 +31,6 @@ export default () => {
           </Switch>
         </main>
       </div>
-
     </div>
-  )
-}
+  );
+};
