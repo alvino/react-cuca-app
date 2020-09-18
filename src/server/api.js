26b7942b64098208896ffc7d61ea79ddaafb8e0e
alvino figueiredo <crazyduckalvino@gmail.com>
2020-09-18 15:47:21 -0300

    configurando variavel de ambiente

--- src/server/api.js
+++ src/server/api.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
-
+import dotenv from 'dotenv'
+dotenv.config()
 // 'https://cuca-api.herokuapp.com'
 // 'http://localhost:3333'
 
 
 const instance = axios.create({
-  baseURL: "https://cuca-api.herokuapp.com",
+  baseURL: process.env.API_CUCA,
 });
 
 export default instance
