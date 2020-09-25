73a0b244b3b38cd0edfe1dcc0ff46380a7be04c9
alvino figueiredo <crazyduckalvino@gmail.com>
2020-09-25 00:47:43 -0300

    corrigindo funcionalidade no estoque

--- src/pages/estoque/register.jsx
+++ src/pages/estoque/register.jsx
@@ -20,10 +20,10 @@ export default () => {
 
   const [providers, setProviders] = useState([]);
   const [selectedFornecedor, setSelectedFornecedor] = useState({});
-   
-  const descriptionInputRef = useRef()
-  const detailInputRef = useRef()
-  const unitInputRef = useRef()
+
+  const descriptionInputRef = useRef();
+  const detailInputRef = useRef();
+  const unitInputRef = useRef();
   const [quantity, setQuantity] = useState({
     formattedValue: "0",
     value: "0",
@@ -59,7 +59,6 @@ export default () => {
     }
 
     const newStock = {
-      index: listaProdutos.length + 1,
       provider_id: selectedFornecedor.id,
       description: descriptionInputRef.current.value,
       detail: detailInputRef.current.value,
@@ -79,11 +78,21 @@ export default () => {
       return;
     }
 
+    const res = await api.post("/stock", newStock);
+    if (res.status > 500) {
+      toast.error(res.data.message);
+      return;
+    }
+
+    toast.success(res.data.message);
+    newStock.id = res.data.stock_id;
+    newStock.index = listaProdutos.length + 1;
+
     setListaProdutos([newStock, ...listaProdutos]);
-    
-    descriptionInputRef.current.value = ''
-    detailInputRef.current.value = ''
-    unitInputRef.current.value = ''
+
+    descriptionInputRef.current.value = "";
+    detailInputRef.current.value = "";
+    unitInputRef.current.value = "";
     setQuantity({ formattedValue: "0", value: "0", floatValue: 0 });
     setPurchasePrice({ formattedValue: "0", value: "0", floatValue: 0 });
     setSaleValue({ formattedValue: "0", value: "0", floatValue: 0 });
@@ -93,177 +102,172 @@ export default () => {
     setSelectedFornecedor(selected);
   };
 
-  const handleSaveListStock = async () => {
-    const res = await api.post(
-      "/stock",
-      listaProdutos.map(item => ({
-        provider_id: selectedFornecedor.id,
-        description: descriptionInputRef.current.value,
-        detail: detailInputRef.current.value,
-        unit: unitInputRef.current.value,
-        quantity: quantity.floatValue,
-        purchase_price: purchasePrice.floatValue,
-        sale_value: saleValue.floatValue,
-      }))
-    );
-    toast.success(res.data.message);
-    history.goBack();
-  };
+  const handleRemoveProduto = async produto => {
+     const res = await api.delete(`stock/${produto.id}`);
+    if (res.status > 500) {
+      toast.error(res.data.message);
+      return;
+    }
 
-  
+    toast.success(res.data.message);
+
+     setListaProdutos(listaProdutos.filter((stock) => stock !== produto));
+  }
 
   return (
-    <div className="row">
-      <div className="col-4">
-        <InputFormControl
-          label="Fornecedor"
-          id="inputFornecedor"
-          name="inputFornecedor"
-          value={selectedFornecedor.nickname || ""}
-          readOnly
-        >
-          <ModalCenterBootstrapTable
-            title="Lista de Fornecedores"
-            data={providers}
-            onSelected={handleSelectedFornecedor}
+    <>
+      <div className="row mb-2">
+        <button className="btn btn-secondary" onClick={() => history.goBack()}>
+          Voltar
+        </button>
+      </div>
+      <div className="row">
+        <div className="col-4">
+          <InputFormControl
+            label="Fornecedor"
+            id="inputFornecedor"
+            name="inputFornecedor"
+            value={selectedFornecedor.nickname || ""}
+            readOnly
           >
-            <TableHeaderColumn dataField="id" isKey width="10%">
+            <ModalCenterBootstrapTable
+              title="Lista de Fornecedores"
+              data={providers}
+              onSelected={handleSelectedFornecedor}
+            >
+              <TableHeaderColumn dataField="id" isKey width="10%">
+                #
+              </TableHeaderColumn>
+              <TableHeaderColumn dataField="nickname">
+                Descrição
+              </TableHeaderColumn>
+              <TableHeaderColumn dataField="cnpj" width="20%">
+                CNPJ/CPF
+              </TableHeaderColumn>
+            </ModalCenterBootstrapTable>
+            <button
+              className="btn btn-secondary"
+              onClick={() => history.push("/fornecedor/register")}
+            >
+              Criar
+            </button>
+          </InputFormControl>
+
+          <div className="form-group">
+            <label htmlFor="inputDescription">Descrição</label>
+            <input
+              type="text"
+              className="form-control"
+              id="inputDescription"
+              ref={descriptionInputRef}
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="inputDetail">Detalhe</label>
+            <input
+              type="text"
+              className="form-control"
+              id="inputDetail"
+              ref={detailInputRef}
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="inputUnit">Unidade de Medida</label>
+            <input
+              type="text"
+              className="form-control"
+              id="inputUnit"
+              ref={unitInputRef}
+            />
+          </div>
+
+          <InputNumberFormat
+            label="Quantidade"
+            id="quantity"
+            name="quantity"
+            prefix=""
+            value={quantity.formattedValue}
+            onValueChange={(values) => setQuantity(values)}
+          />
+
+          <InputNumberFormat
+            label="Valor da Compra"
+            id="purchase_price"
+            name="purchase_price"
+            value={purchasePrice.formattedValue}
+            onValueChange={(values) => setPurchasePrice(values)}
+          />
+
+          <InputNumberFormat
+            label="Valor de Venda"
+            id="sale_value"
+            name="sale_value"
+            value={saleValue.formattedValue}
+            onValueChange={(values) => setSaleValue(values)}
+          />
+
+          <div className="d-flex justify-content-end">
+            <Button variant="success" onClick={handleSubmit}>
+              Adicionar
+            </Button>
+          </div>
+        </div>
+        <div className="col-8">
+          <BootstrapDataTable
+            data={listaProdutos}
+            pagination={false}
+            search={false}
+            exportCSV={false}
+          >
+            <TableHeaderColumn
+              dataField="index"
+              width="8%"
+              isKey
+              dataFormat={(cell, row) => (
+                <span
+                  onClick={() => 
+
+                   handleRemoveProduto(row)
+                  }
+                >
+                  <IconRemoveList
+                    title="Revomer produto da lista"
+                    color="red"
+                  />
+                </span>
+              )}
+            >
               #
             </TableHeaderColumn>
-            <TableHeaderColumn dataField="nickname">
-              Descrição
+            <TableHeaderColumn dataField="description">
+              Produto (descrição)
             </TableHeaderColumn>
-            <TableHeaderColumn dataField="cnpj" width="20%">
-              CNPJ/CPF
+            <TableHeaderColumn
+              dataField="quantity"
+              width="10%"
+              dataFormat={numberFormatter}
+            >
+              Qt.
             </TableHeaderColumn>
-          </ModalCenterBootstrapTable>
-          <button
-            className="btn btn-secondary"
-            onClick={() => history.push("/fornecedor/register")}
-          >
-            Criar
-          </button>
-        </InputFormControl>
-
-        <div className="form-group">
-          <label htmlFor="inputDescription">Descrição</label>
-          <input
-            type="text"
-            className="form-control"
-            id="inputDescription"
-            ref={descriptionInputRef}
-          />
+            <TableHeaderColumn
+              dataField="purchase_price"
+              width="15%"
+              dataFormat={priceFormatter}
+            >
+              R$ Compra
+            </TableHeaderColumn>
+            <TableHeaderColumn
+              dataField="sale_value"
+              width="15%"
+              dataFormat={priceFormatter}
+            >
+              R$ Venda
+            </TableHeaderColumn>
+          </BootstrapDataTable>
         </div>
-
-        <div className="form-group">
-          <label htmlFor="inputDetail">Detalhe</label>
-          <input
-            type="text"
-            className="form-control"
-            id="inputDetail"
-            ref={detailInputRef}
-          />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="inputUnit">Unidade de Medida</label>
-          <input
-            type="text"
-            className="form-control"
-            id="inputUnit"
-            ref={unitInputRef}
-          />
-        </div>
-
-        <InputNumberFormat
-          label="Quantidade"
-          id="quantity"
-          name="quantity"
-          prefix=""
-          value={quantity.formattedValue}
-          onValueChange={(values) =>
-            setQuantity(values)
-          }
-        />
-
-        <InputNumberFormat
-          label="Valor da Compra"
-          id="purchase_price"
-          name="purchase_price"
-          value={purchasePrice.formattedValue}
-          onValueChange={(values) => setPurchasePrice(values)}
-        />
-
-        <InputNumberFormat
-          label="Valor de Venda"
-          id="sale_value"
-          name="sale_value"
-          value={saleValue.formattedValue}
-          onValueChange={(values) => setSaleValue(values)}
-        />
-
-        <div className="d-flex justify-content-end">
-          <Button variant="success" onClick={handleSubmit}>
-            Adicionar
-          </Button>
-        </div>
-        {listaProdutos.length === 0 || (
-          <Button variant="primary" onClick={handleSaveListStock}>
-            Salvar Lista de Produtos
-          </Button>
-        )}
       </div>
-      <div className="col-8">
-        <BootstrapDataTable
-          data={listaProdutos}
-          pagination={false}
-          search={false}
-          exportCSV={false}
-        >
-          <TableHeaderColumn
-            dataField="index"
-            width="8%"
-            isKey
-            dataFormat={(cell, row) => (
-              <span
-                onClick={() =>
-                  setListaProdutos(
-                    listaProdutos.filter((stock) => stock !== row)
-                  )
-                }
-              >
-                <IconRemoveList title="Revomer produto da lista" color="red" />
-              </span>
-            )}
-          >
-            #
-          </TableHeaderColumn>
-          <TableHeaderColumn dataField="description">
-            Produto (descrição)
-          </TableHeaderColumn>
-          <TableHeaderColumn
-            dataField="quantity"
-            width="10%"
-            dataFormat={numberFormatter}
-          >
-            Qt.
-          </TableHeaderColumn>
-          <TableHeaderColumn
-            dataField="purchase_price"
-            width="15%"
-            dataFormat={priceFormatter}
-          >
-            R$ Compra
-          </TableHeaderColumn>
-          <TableHeaderColumn
-            dataField="sale_value"
-            width="15%"
-            dataFormat={priceFormatter}
-          >
-            R$ Venda
-          </TableHeaderColumn>
-        </BootstrapDataTable>
-      </div>
-    </div>
+    </>
   );
 };
